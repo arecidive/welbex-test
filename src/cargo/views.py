61a77d8c254d10c464ff7023e3d9e69585366608
arecidive <anton.kimyaev@mail.ru>
2024-03-26 21:55:00 +0300

    Create ViewSet for 'Cargo' model and create urls for ViewSet.

---
+++ src/cargo/views.py
@@ -0,0 +1,28 @@
+from rest_framework.serializers import ModelSerializer
+from rest_framework.viewsets import ModelViewSet
+
+from . import serializers
+from .models import Cargo
+
+
+class CargoViewSet(ModelViewSet):
+    """
+    Класс является ViewSet для управления грузами.
+    """
+
+    queryset = Cargo.objects.all()
+
+    def get_serializer_class(self) -> ModelSerializer:
+        """
+        Метод возвращает класс сериализатора в зависимости от выполняемого действия.
+
+        :returns: класс сериализатора.
+        """
+
+        if self.action == 'create':
+            return serializers.CargoCreateSerializer
+        if self.action == 'list':
+            return serializers.CargoListSerializer
+        if self.action == 'retrieve':
+            return serializers.CargoRetrieveSerializer
+        return serializers.CargoUpdateSerializer
