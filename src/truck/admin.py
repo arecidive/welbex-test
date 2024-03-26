f95aa7ede749200e4e0a3c9161d9d7ff691f236d
arecidive <anton.kimyaev@mail.ru>
2024-03-26 22:34:28 +0300

    Optimize admin panel for model 'Truck'.

--- src/truck/admin.py
+++ src/truck/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Truck
 
-admin.site.register(Truck)
+
+@admin.register(Truck)
+class TruckAdmin(admin.ModelAdmin):
+    """
+    Административная конфигурация для модели 'Truck'.
+    """
+
+    list_display = ('number', 'current_location', 'capacity')
+    raw_id_fields = ('current_location',)
