3df0c85232bbf702e9399e1aa8c2d80e8abcd7f8
arecidive <anton.kimyaev@mail.ru>
2024-03-26 13:54:23 +0300

    Add model 'Location' in admin. Add docstring and edit app in 'apps.py'.

--- src/location/apps.py
+++ src/location/apps.py
@@ -2,5 +2,9 @@ from django.apps import AppConfig
 
 
 class LocationConfig(AppConfig):
+    """
+    Класс представляет собой конфигурацию приложения 'location' в Django.
+    """
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'location'
