09f77f7a2cfbd076dcd2ebe35e811da131563bbf
arecidive <anton.kimyaev@mail.ru>
2024-03-26 22:00:00 +0300

    Optimize locations in admin panel

--- src/cargo/admin.py
+++ src/cargo/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Cargo
 
-admin.site.register(Cargo)
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('pick_up_location', 'delivery_location', 'weight', 'description')
+    raw_id_fields = ('pick_up_location', 'delivery_location')
