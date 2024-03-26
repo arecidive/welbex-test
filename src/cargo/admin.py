ae1931e7d3b696a117beb82883c04e4c5edea2e8
arecidive <anton.kimyaev@mail.ru>
2024-03-26 22:01:07 +0300

    Add docstring in 'CargoAdmin'.

--- src/cargo/admin.py
+++ src/cargo/admin.py
@@ -5,5 +5,9 @@ from .models import Cargo
 
 @admin.register(Cargo)
 class CargoAdmin(admin.ModelAdmin):
+    """
+    Административная конфигурация для модели 'Cargo'.
+    """
+
     list_display = ('pick_up_location', 'delivery_location', 'weight', 'description')
     raw_id_fields = ('pick_up_location', 'delivery_location')
