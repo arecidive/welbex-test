53683170a9bac755b5b22d0ee03f74b59f21aa68
Anton Kimyaev <anton.kimyaev@mail.ru>
2024-03-29 12:56:31 +0300

    Add docstring for apps.py in 'api' app.

--- src/api/apps.py
+++ src/api/apps.py
@@ -2,5 +2,9 @@ from django.apps import AppConfig
 
 
 class ApiConfig(AppConfig):
+    """
+    Класс представляет собой конфигурацию приложения 'api' в Django.
+    """
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
